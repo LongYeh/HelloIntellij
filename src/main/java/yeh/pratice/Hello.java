096928771060a7e2b38220c366b91df075f0e05a
Mr.YEH <zxc09267520@gmail.com>
2019-08-20 20:50:38 +0800

    java and kotlin variable,data type.

--- src/main/java/yeh/pratice/Hello.java
+++ src/main/java/yeh/pratice/Hello.java
@@ -4,6 +4,17 @@ public class Hello {
     public static void main(String[] args) {
 
         new Person().Hello();
+        int i=4;
+        char c='L';
+        byte b=127;
+        float f=12.7f;
+        double d=12.7;
+        long l=4L;
+        boolean B=true;
+        String s="小麻糬";
+//        alt+9
+//        ctrl+d
+//        alt+`
 
     }
 
--- src/main/kotlin/yeh/pratice/Hello.kt
+++ src/main/kotlin/yeh/pratice/Hello.kt
@@ -5,6 +5,13 @@ fun main() {
 //    Person().hello();
     val h=Person();
     h.hello();
+//    val 不可改變
+//    var 可改變
+//    var  age:Int =123;
+    var age=123;
+    var kg:Double=65.5;
+    var name:String;
+    name="小麻糬";
 }
 
 class Person{
